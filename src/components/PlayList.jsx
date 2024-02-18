31e141fbe201446b678ad64d33eeb11b2c80a0cb
Jijesh K V <contactjijeshkv@gmail.com>
2024-02-18 16:56:50 +0530

    final commit

--- src/components/PlayList.jsx
+++ src/components/PlayList.jsx
@@ -1,21 +1,40 @@
-
+import { useState } from 'react';
 import { Card } from './Card';
 import data from '../../data.js';
 
 const PlayList = () => {
+  const [sortedData, setSortedData] = useState([...data.categories[0].videos]);
+  const [isAscending, setIsAscending] = useState(true);
+
+  const handleSort = () => {
+    const sorted = [...sortedData].sort((a, b) => {
+      const titleA = a.title.toUpperCase();
+      const titleB = b.title.toUpperCase();
+      if (isAscending) {
+        return titleA.localeCompare(titleB);
+
+      } else {
+        return titleB.localeCompare(titleA);
+      }
+    });
+
+    setSortedData(sorted);
+    setIsAscending(!isAscending);
+  };
 
   return (
     <div className="w-full bg-white h-auto">
-      <div className='max-w-[90%] mx-auto mt-2'>
-        <button className="ml-3 bg-transparent hover:bg-blue-500 text-black font-semibold hover:text-white py-1 px-5 border border-blue-500 hover:border-transparent rounded">
-          A-Z
-        </button>
-        <button className="bg-transparent m-2 hover:bg-blue-500 text-black font-semibold hover:text-white py-1 px-5 border border-blue-500 hover:border-transparent rounded">
-          Z-A
+      <div className='flex max-w-[90%] mx-auto mt-2'>
+        <p className='text-xl ml-2'>Order By:</p>
+        <button
+          className="ml-3 bg-transparent hover:bg-blue-500 text-black font-semibold hover:text-white py-1 px-5 border border-blue-500 hover:border-transparent rounded"
+          onClick={handleSort}
+        >
+          Alphabeticaly
         </button>
       </div>
       <div className="flex flex-wrap max-w-[90%] mx-auto justify-between">
-        {data.categories[0].videos.map((video) => (
+        {sortedData.map((video) => (
           <Card key={Math.random()} data={video} />
         ))}
       </div>
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,14 +1,16 @@
 import { useSelector } from "react-redux"
 
-const VideoPlayer = (video) => {
+const VideoPlayer = () => {
   const videoUrl = useSelector((state) => state.video.video.sources)
 
   return (
     <>
-      <video controls autoPlay width={'800rem'} height={'700rem'}  src={videoUrl[0]}></video>
+      <video controls autoPlay width={'800rem'} height={'700rem'}  src={videoUrl[0]}>
+      </video>
 
     </>
   )
 }
 
 export default VideoPlayer
+
